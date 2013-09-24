e064a50981553e21421198a51d2488a64b5fae48
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2013-09-24 15:24:07 +0400

    some quick hacks to avoid segfaults for now

--- pypyodbc.py
+++ pypyodbc.py
@@ -1800,6 +1800,9 @@ class Cursor:
                                     value_list.append(buf_cvt_func(alloc_buffer.raw[:used_buf_len.value]))
                                 elif target_type == SQL_C_WCHAR:
                                     value_list.append(buf_cvt_func(from_buffer_u(alloc_buffer)))
+                                elif alloc_buffer.value == '':
+                                    # FIXME - fails with ValueError
+                                    value_list.append(None)
                                 else:
                                     #print col_name, target_type, alloc_buffer.value
                                     value_list.append(buf_cvt_func(alloc_buffer.value))
@@ -2429,6 +2432,7 @@ class Connection:
         return cur
 
     def update_db_special_info(self):
+        return # FIXME - to avoid segfault in cur.getTypeInfo
         for sql_type in (
             SQL_TYPE_TIMESTAMP,
             SQL_TYPE_DATE,
