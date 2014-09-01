37d7f7ff10340a0e063e8bcd802f7474b57c711d
Pavel Batishchev <pbatishchev@gmail.com>
2014-09-01 17:35:39 +0400

    do not convert strings to datetime, date and time

--- pypyodbc.py
+++ pypyodbc.py
@@ -565,21 +565,19 @@ def dttm_cvt(x):
     if py_v3:
         x = x.decode('ascii')
     if x == '': return None
-    x = x.ljust(26,'0')
-    return datetime.datetime(int(x[0:4]),int(x[5:7]),int(x[8:10]),int(x[10:13]),int(x[14:16]),int(x[17:19]),int(x[20:26]))
+    return x.ljust(26,'0')
 
 def tm_cvt(x):
     if py_v3:
         x = x.decode('ascii')
     if x == '': return None
-    x = x.ljust(15,'0')
-    return datetime.time(int(x[0:2]),int(x[3:5]),int(x[6:8]),int(x[9:15]))
+    return x.ljust(15,'0')
 
 def dt_cvt(x):
     if py_v3:
         x = x.decode('ascii')
     if x == '': return None
-    else: return datetime.date(int(x[0:4]),int(x[5:7]),int(x[8:10]))
+    return x
 
 def Decimal_cvt(x):
     if py_v3:
